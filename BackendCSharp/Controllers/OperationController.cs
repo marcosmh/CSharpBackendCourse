47f043f2d700e5086c8a519b4bebd2c63461c81a
Marcos Magaña <imarcosmagana@gmail.com>
2026-03-16 23:18:13 -0600

    Backend en C# .NET - Controladores -  actualizacion

--- BackendCSharp/Controllers/OperationController.cs
+++ BackendCSharp/Controllers/OperationController.cs
@@ -10,12 +10,41 @@ namespace BackendCSharp.Controllers
 	{
 
 		[HttpGet]
-		public decimal Add(decimal a, decimal b)
+		public decimal Get(decimal a, decimal b)
 		{
 			return a + b;
 		}
 
+		[HttpPost]
+		public decimal Add(Numbers numbers, [FromHeader] string Host,
+			[FromHeader(Name ="Content-Length")] string contentLength,
+			[FromHeader(Name ="X-some")] string some)
+		{
+			Console.WriteLine(Host);
+			Console.WriteLine(contentLength);
+			Console.WriteLine(some);
+
+			return numbers.a - numbers.b;
+		}
+
+		[HttpPut]
+		public decimal Edit(decimal a, decimal b)
+		{
+			return a / b;
+		}
+
+		[HttpDelete]
+		public decimal Delete(decimal a, decimal b)
+		{
+			return a * b;
+		}
 
 	}
 }
 
+public class Numbers
+{
+	public decimal a { get; set; }
+
+	public decimal b { get; set; }
+}
