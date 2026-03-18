937f17021a28e37ef6fbf621feb32f3d3889e3aa
Marcos Magaña <imarcosmagana@gmail.com>
2026-03-17 23:17:30 -0600

    Backend en C# .NET - Controladores -  respuestas http

---
+++ BackendCSharp/Controllers/PeopleController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BackendCSharp.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("all")]
+        public List<People> GetPeople() => Repository.People;
+
+        //[HttpGet("{id}")]
+        //public People Get(int id) => Repository.People.First( p => p.Id == id );
+
+        [HttpGet("{id}")]
+        public ActionResult<People> Get(int id)
+        {
+            var people = Repository.People.FirstOrDefault(p => p.Id == id);
+
+            if(people == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(people);
+        }
+
+        [HttpGet("search/{search}")]
+        public List<People> Get(string search) =>
+            Repository.People.Where(p => p.Name.ToUpper().Contains(search.ToUpper())).ToList();
+
+        [HttpPost]
+        public IActionResult Add(People people)
+        {
+            if(string.IsNullOrEmpty(people.Name))
+            {
+                return BadRequest();
+            }
+
+            Repository.People.Add(people);
+
+            return NoContent();
+        }
+    }
+
+}
+
+
+public class Repository
+{
+    public static List<People> People = new List<People>
+    {
+        new People() { Id=1, Name="Anakin", BirthDate= new DateTime(1970,03,01) },
+        new People() { Id=2, Name="Luke", BirthDate= new DateTime(1990,03,01) },
+        new People() { Id=3, Name="Obi Wan", BirthDate= new DateTime(1960,06,22) },
+        new People() { Id=4, Name="Leia", BirthDate= new DateTime(1990,03,01) }
+    };
+}
+
+
+public class People
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+}
+
+
